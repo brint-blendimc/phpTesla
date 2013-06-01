7c9310a9f76071d4733096a87ae5bbde52a9de02
Brint <phptesla@gmail.com>
2013-06-01 09:06:30 -0500

    Created a new comment structure.

--- classes/File.php
+++ classes/File.php
@@ -13,20 +13,15 @@
 * File::setPermissions($filePath, $permMode = 0755)		// Sets the permission mode of a file.
 */
 
-abstract class File
-{
-	/****** Read File Contents ******
-	* This method retrieves the contents of a file and returns them as a string.
-	*
-	****** How to call the method ******
-	* File::read("/path/to/file/myfile.txt");
-	* 
-	****** Parameters ******
-	* @string	$filePath		The full file path of the file to read.
-	* 
-	* RETURNS <string>			Returns the contents of the file (empty string if it doesn't exist).
-	*/
-	public static function read($filepath)
+abstract class File {
+
+/****** Read File Contents ******/
+	public static function read
+	(
+		$filepath		/* <str> The full file path of the file to read. */
+	)					/* RETURNS <str> : Contents of the file (empty string if it doesn't exist). */
+	
+	// File::read("/path/to/file/myfile.txt");
 	{
 		if(file_exists($filepath) && !is_dir($filepath))
 		{
@@ -36,42 +31,36 @@ abstract class File
 		return '';
 	}
 	
-	/****** Create a File ******
+/****** Create a File ******
 	* This method creates a file with the desired content in it. If the file already exists, it will return false.
 	*
 	* Note: this file will attempt to create the directories leading up to the file if they do not currently exist.
-	*
-	****** How to call the method ******
-	* File::create("/path/to/file/myfile.txt", "Some content that I would like to write to this file.");
-	* 
-	****** Parameters ******
-	* @string	$filePath		The full file path of the file to create.
-	* @string	$text			The text to include in the file.	
-	* 
-	* RETURNS <bool>			Returns TRUE if created properly, FALSE if something went wrong.
 	*/
-	public static function create($filepath, $text)
+	public static function create
+	(
+		$filepath		/* <str> The full file path of the file to create. */,
+		$text			/* <str> The text to include in the file. */
+	)					/* RETURNS <bool> : TRUE if updated properly, FALSE if something went wrong. */
+	
+	// File::create("/path/to/file/myfile.txt", "Some content that I would like to write to this file.");
 	{
 		return self::write($filepath, $text, false);
 	}
 	
-	/****** Write to a File ******
+/****** Write to a File ******
 	* This method overwrites a file's content with content of your own. If the file doesn't exist, it creates it. The
 	* permissions need to be valid in order to write to this file.
 	*
 	* Note: this file will attempt to create the directories leading up to the file if they do not currently exist.
-	*
-	****** How to call the method ******
-	* File::write("/path/to/file/myfile.txt", "Some content that I would like to write to this file.");
-	* 
-	****** Parameters ******
-	* @string	$filePath		The full file path of the file to write.
-	* @string	$text			The text to include in the file.
-	* ?bool		$overwrite		Sets whether or not this function should overwrite an existing file.
-	* 
-	* RETURNS <bool>			Returns TRUE if updated properly, FALSE if something went wrong.
 	*/
-	public static function write($filepath, $text, $overwrite = true)
+	public static function write
+	(
+		$filepath			/* <str> The full file path of the file to write. */,
+		$text				/* <str> The text to include in the file. */,
+		$overwrite = true	/* <bool> Sets whether or not this function should overwrite existing files. */
+	)						/* RETURNS <bool> : TRUE if updated properly, FALSE if something went wrong. */
+	
+	// File::write("/path/to/file/myfile.txt", "Some content that I would like to write to this file.");
 	{
 		// Check if the file already exists, and react accordingly
 		if(file_exists($filepath) && $overwrite == false)
@@ -92,69 +81,49 @@ abstract class File
 		return file_put_contents($filepath, $text);
 	}
 	
-	/****** Delete a File ******
-	* This method deletes a file.
-	*
-	****** How to call the method ******
-	* File::delete("/path/to/file/myfile.txt");
-	* 
-	****** Parameters ******
-	* @string	$filePath		The full file path of the file to delete.
-	* 
-	* RETURNS <bool>			Returns TRUE on success, FALSE on failure.
-	*/
-	public static function delete($filepath)
+/****** Delete a File ******/
+	public static function delete
+	(
+		$filepath		/* <str> The full file path of the file to delete. */
+	)					/* Returns TRUE on success, FALSE on failure. */
+	
+	// File::delete("/path/to/file/myfile.txt");
 	{
 		return unlink($filepath);
 	}
 	
-	/****** Copy a File ******
-	* This method copies a file from one directory to another.
-	*
-	****** How to call the method ******
-	* File::copy("/path/to/file/myfile.txt", "/new/path/myfile.txt");
-	* 
-	****** Parameters ******
-	* @string	$fromPath		The full file path of the file to copy.
-	* @string	$toPath			The full file path of the new location to copy/clone the oriinal file.
-	* 
-	* RETURNS <bool>			Returns TRUE on success, FALSE on failure.
-	*/
-	public static function copy($fromPath, $toPath)
+/****** Copy a File ******/
+	public static function copy
+	(
+		$fromPath		/* <str> The full file path of the file to copy. */,
+		$toPath			/* <str> The full file path of the new location to copy/clone the original file. */
+	)					/* Returns TRUE on success, FALSE on failure. */
+	
+	// File::copy("/path/to/file/myfile.txt", "/new/path/myfile.txt");
 	{
 		return copy($fromPath, $toPath);
 	}
 	
-	/****** Move a File ******
-	* This method moves a file from one directory to another.
-	*
-	****** How to call the method ******
-	* File::move("/path/to/file/myfile.txt", "/new/path/myfile.txt");
-	* 
-	****** Parameters ******
-	* @string	$fromPath		The full file path of the file to move.
-	* @string	$toPath			The full file path of the new location of the oriinal file.
-	* 
-	* RETURNS <bool>			Returns TRUE on success, FALSE on failure.
-	*/
-	public static function move($fromPath, $toPath)
+/****** Move a File ******/
+	public static function move
+	(
+		$fromPath		/* <str> The full file path of the file to move. */,
+		$toPath			/* <str> The full file path of the new location - i.e. where you're moving the original file to. */
+	)					/* <bool> Returns TRUE on success, FALSE on failure. */
+	
+	// File::move ("/path/to/file/myfile.txt", "/new/path/myfile.txt");
 	{
 		return rename($fromPath, $toPath);
 	}
 	
-	/****** Set Permissions of a File ******
-	* This method sets the permission mode of a file.
-	*
-	****** How to call the method ******
-	* File::setPermissions("/path/to/file/myfile.txt", 0755);	// Sets permissions to 0755
-	* 
-	****** Parameters ******
-	* @string	$filePath			The full file path of the file to set permissions on.
-	* ?int		$permissionMode		The number used to set the permission mode. (i.e. 0755, 755)
-	* 
-	* RETURNS <bool>			Returns TRUE on success, FALSE on failure.
-	*/
-	public static function setPermissions($filePath, $permissionMode = 0755)
+/****** Set Permissions of a File ******/
+	public static function setPermissions
+	(
+		$filePath					/* <str> The full file path of the file to set permissions on. */,
+		$permissionMode = 0755		/* <int> The number used to set the permission mode. (i.e. 0755, 755, etc) */
+	)								/* Returns TRUE on success, FALSE on failure. */
+	
+	// File::setPermissions("/path/to/file/myfile.txt", 0755);	// Sets permissions to 0755
 	{
 		// Make sure the file exists and isn't a directory
 		if(!file_exist($filePath) || is_dir($filePath))
